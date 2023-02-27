32f8d02f9b9d65540bae1ac19df91bbfc0d7e774
Cioarec Stelian <legoninjagoproductions318@gmail.com>
2023-02-27 20:43:48 +0200

    Updated info.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,48 @@
 const theme = localStorage.getItem('theme');
+const avatar = localStorage.getItem('avatar');
+const username = localStorage.getItem('username');
+const usernameItem = document.getElementById('username');
+const avatarImage = document.getElementById('avatar');
 
 if (theme === 'dark') {
     document.body.classList.add('dark');
 }
+
+if (avatar === 'default') {
+    avatarImage.src = "/images/avatar/default.jpg";
+} else if (avatar === 'lego') {
+    avatarImage.src = "/images/avatar/lego.jpg";
+} else if (avatar === 'lnp-group') {
+    avatarImage.src = "/images/avatar/lnp-group.jpg";
+} else if (avatar === 'lnp-productions') {
+    avatarImage.src = "/images/avatar/lnp-productions.jpg";
+} else if (avatar === 'ninjago') {
+    avatarImage.src = "/images/avatar/ninjago.jpg";
+} else if (avatar === 'lloyd') {
+    avatarImage.src = "/images/avatar/lloyd.jpg";
+} else if (avatar === 'kai') {
+    avatarImage.src = "/images/avatar/kai.jpg";
+} else if (avatar === 'zane') {
+    avatarImage.src = "/images/avatar/zane.jpg";
+} else if (avatar === 'jay') {
+    avatarImage.src = "/images/avatar/jay.jpg";
+} else if (avatar === 'cole') {
+    avatarImage.src = "/images/avatar/cole.jpg";
+} else if (avatar === 'nya') {
+    avatarImage.src = "/images/avatar/nya.jpg";
+} else if (avatar === 'master-wu') {
+    avatarImage.src = "/images/avatar/master-wu.jpg";
+} else {
+    avatarImage.src = "/images/avatar/default.jpg";
+}
+
+username.setItem('Guest');
+
+if (username === 'Guest') {
+    usernameItem.innerHTML = 'Guest';
+} else {
+    usernameItem.innerHTML = username;
+}
+
+
 
--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -1,6 +1,9 @@
 const language = document.getElementById('language');
 const langPicker = document.getElementById('language-picker');
 const theme = document.getElementById('theme');
+const avatar = document.getElementById('avatar');
+const userName = document.getElementById('username');
+const userNameForm = document.getElementById('username-form');
 
 theme.addEventListener('change', () => {
     localStorage.setItem('theme', theme.value);
@@ -17,5 +20,62 @@ if (localStorage.getItem('theme') === null) {
     document.body.classList.add('dark');
     theme.value = 'dark';
 }
+
+if (localStorage.getItem('avatar') === null) {
+    localStorage.setItem('avatar', 'default');
+} else if (avatar.value === 'default') {
+    localStorage.setItem('avatar', 'default');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'lego') {
+   localStorage.setItem('avatar', 'lego');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'lnp-group') {
+    localStorage.setItem('avatar', 'lnp-group');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'lnp-productions') {
+    localStorage.setItem('avatar', 'lnp-productions');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'ninjago') {
+    localStorage.setItem('avatar', 'ninjago');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'lloyd') {
+    localStorage.setItem('avatar', 'lloyd');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'kai') {
+    localStorage.setItem('avatar', 'kai');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'zane') {
+    localStorage.setItem('avatar', 'zane');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'jay') {
+    localStorage.setItem('avatar', 'jay');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'cole') {
+    localStorage.setItem('avatar', 'cole');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'nya') {
+    localStorage.setItem('avatar', 'nya');
+    avatar.value = localStorage.getItem('avatar');
+} else if (avatar.value === 'master-wu') {
+    localStorage.setItem('avatar', 'master-wu');
+    avatar.value = localStorage.getItem('avatar');
+} else {
+    localStorage.setItem('avatar', 'default');
+    avatar.value = localStorage.getItem('avatar');
+}
+
+if (localStorage.getItem('username') === null) {
+    localStorage.setItem('username', 'Guest');
+}
+
+userNameForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    localStorage.setItem('username', userName.value.toString());
+    window.location.reload();
+});
+
+console.log('The avatar of the user is ' + localStorage.getItem('avatar'));
+
+console.log('The username of the user is ' + localStorage.getItem('username'));
 
 console.log('The theme of the website is ' + localStorage.getItem('theme'));
